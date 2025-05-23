5bd39c9946dbe84f417c9a628c019823838f6732
jerry <swms22@naver.com>
2025-05-23 15:33:39 +0900

    Fix: 컴포넌트 구조 변경

--- src/pages/editSummary/EditSummary.tsx
+++ src/pages/editSummary/EditSummary.tsx
@@ -1,13 +1,36 @@
-import React from "react";
 import { PageWrap } from "@/components/PageWrap/PageWrap";
-import PDFEditorApp from "@/components/PDFEditor/PdfEditorApp";
+import { Link, Outlet, useLocation, useNavigate } from "@tanstack/react-router";
+import { NavWrap, NavButton, EditWrap } from "./EditSummary.styles";
+import { useEffect } from "react";
 
-export const EditSummary: React.FC = () => {
+export const EditSummary = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const currentPath = location.pathname;
+  const view = currentPath.includes("/edit/contents")
+    ? "contents"
+    : "summation";
+
+  useEffect(() => {
+    if (location.pathname === "/edit") {
+      navigate({ to: "/edit/contents", replace: true });
+    }
+  }, [location.pathname, navigate]);
+
   return (
     <PageWrap>
-      <PDFEditorApp />
+      <EditWrap>
+        <NavWrap>
+          <Link to="/edit/contents">
+            <NavButton $isActive={view === "contents"}>콘텐츠 수정</NavButton>
+          </Link>
+          <Link to="/edit/summation">
+            <NavButton $isActive={view === "summation"}>요약 수정</NavButton>
+          </Link>
+        </NavWrap>
+        <Outlet />
+      </EditWrap>
     </PageWrap>
   );
 };
-
-export default EditSummary;
