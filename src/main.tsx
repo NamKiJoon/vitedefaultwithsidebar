1e3723935d9443806a22fb0b24ed7babd4cdf105
jerry <swms22@naver.com>
2025-05-23 15:29:33 +0900

    Feat: Query Client 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router/router";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  //   <QueryClientProvider client={queryClient}>
+  //     <RouterProvider router={router} />
+  //   </QueryClientProvider>
+  // </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
-  </React.StrictMode>
-  // <RouterProvider router={router} />
+  </QueryClientProvider>
 );
